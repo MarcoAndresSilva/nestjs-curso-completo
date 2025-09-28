a4750bd13bc280001c63089e52f4beae4baa8810
Marco Silva <marco.silvaponce10@gmail.com>
2025-09-28 02:04:29 -0300

    feat(cats): create Cat DTO and use it in service to test cat creation 🐱

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
 
 @Injectable()
 export class CatsService {
-  create(createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+  constructor(
+    @InjectRepository(Cat)
+    private catRepository: Repository<Cat>,
+  ) {}
+
+  async create(createCatDto: CreateCatDto) {
+    return await this.catRepository.save(createCatDto); //'This action adds a new cat';
   }
 
-  findAll() {
-    return `This action returns all cats`;
+  async findAll() {
+    return await this.catRepository.find();
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} cat`;
   }
 
-  update(id: number, updateCatDto: UpdateCatDto) {
+  async update(id: number, updateCatDto: UpdateCatDto) {
     return `This action updates a #${id} cat`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} cat`;
   }
 }
