e7a7e21e53dd3fd835d43a043b7fa607d78dda65
Marco Silva <marco.silvaponce10@gmail.com>
2025-10-02 17:34:03 -0300

    feat(users): add Users module with entity and service configured (controller pending)

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>, //la variable se va comportar como un repositorio y este repositorio tine la entidad de user
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.userRepository.save(createUserDto);
+  }
+
+  findOneByEmail(email: string) {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update(id, updateUserDto);
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
