9cc83f6fc2c53980bf18bc9f93b8d8ca291ea28c
Marco Silva <marco.silvaponce10@gmail.com>
2025-10-03 23:55:32 -0300

    feat(users): implement UsersController and update UsersModule configuration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
-// import { UsersController } from './users.controller';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // para llevar la entidad a la base de datos
-  // controllers: [UsersController],
+  controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
