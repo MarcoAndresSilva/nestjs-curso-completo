6b539c856e5d84b13bfe98fc0dc65df127c0bb47
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-03-07 23:48:04 -0300

    feat: add role admin in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import { Auth } from '../auth/decorators/auth.decorators';
 import { Role } from '../common/enums/rol.enums';
 
 @Auth(Role.ADMIN)
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
